92c7f1ee71ad7971a208c4b6376bb2377d225913
kavitha <kavinu2010@gmail.com>
2025-01-01 19:43:56 +0100

    cart total added

--- store/models.py
+++ store/models.py
@@ -48,6 +48,13 @@ class OrderItem(models.Model):
   order=models.ForeignKey(Order, on_delete=models.SET_NULL,null=True,blank=True)
   quantity=models.IntegerField(default=0,null=True,blank=True)
   date_added=models.DateTimeField(auto_now_add=True)
+ 
+
+  @property
+  def get_total(self):
+    total=self.product.price * self.quantity
+    return total
+
 
 
 class ShippingAddress(models.Model):
