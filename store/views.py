c38dc01d283c8b06c283dff2c6c2eafc7fef219f
kavitha <kavinu2010@gmail.com>
2025-01-01 13:57:37 +0100

    store code updated

--- store/views.py
+++ store/views.py
@@ -9,14 +9,17 @@ def store(request):
 
 def cart(request):
 
-  if request.user.is_authenticated:
-    customer=request.user.customer
-    order, created=Order.objects.get_or_create(customer=customer, complete=False)
-    items=order.orderitem_set.all()
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
 
-
-  context={}
-  return render(request,'store/cart.html',context)
+	context = {'items':items, 'order':order}
+	return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
