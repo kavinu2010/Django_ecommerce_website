98fe17034fe8dcb50037a2439881399973ff376a
kavitha <kavinu2010@gmail.com>
2025-01-04 19:36:48 +0100

    click eventlistener added

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,16 +1,16 @@
 var updateBtns = document.getElementsByClassName("update-cart");
 
-for (i = 0; i < updateBtns.length; i++) {
+for (var i = 0; i < updateBtns.length; i++) {
   updateBtns[i].addEventListener("click", function () {
     var productId = this.dataset.product;
     var action = this.dataset.action;
     console.log("productId:", productId, "Action:", action);
 
-    console.log("USER:", user);
-    if (user === "AnnonymousUSER") {
-      console.log("Not logged in");
-    } else {
-      console.log("user is loged in,sending data");
-    }
+    // console.log("USER:", user);
+    // if (user === "AnnonymousUSER") {
+    //   console.log("Not logged in");
+    // } else {
+    //   console.log("user is loged in,sending data");
+    // }
   });
 }
